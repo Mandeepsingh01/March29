97da0193be856b2cf96e74b6aaac335c943d3556
visualstudio <visualstudio@122-PC23>
2019-03-29 22:12:34 -0400

    Final commit

---
+++ 732152/732152/732152/732152/C0732152_march29/C0732152_march29/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0732152_march29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading file");
+            Download();
+            Console.ReadLine();
+        }
+
+        static async void Download()
+        {
+            await Network.Download();
+            Console.WriteLine("download complete");
+        }
+
+
+        class Network
+        {
+            static ArrayList WebPagesContents = new ArrayList();
+            public static async Task Download();{
+            HttpClient client = new HttpClient();
+            string data = await client.GetStringAssync("http://torontopubliclibrary");
+            Console.WriteLine(data);
+                foreach (var i in data)
+                {
+                WebPageContents.Add(i);
+        }
+    }
+}
+
